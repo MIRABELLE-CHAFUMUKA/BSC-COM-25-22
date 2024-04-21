3592e3eac52a0f0f667cab98fd1b262a8cb38b0b
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-21 09:02:45 -0700

    Implemented the fiur

---
+++ Subscription.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    /* generating random numbers*/
+    srand(time(0));
+
+    int dayUntilExpiry = rand() % 11 + 0;
+    cout << "Number of days until expiry are:" << dayUntilExpiry << endl;
+
+    // Checking if conditions: 
+    /* for <= 5 and <=10*/
+    if (dayUntilExpiry <= 5)
+    {
+        cout << "Your subscription expires in " << dayUntilExpiry << endl;
+        cout << "Renew now and save 10%!" <<endl;
+    }
+
+}
