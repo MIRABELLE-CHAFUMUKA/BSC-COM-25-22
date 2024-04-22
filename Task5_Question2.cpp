69526b9a169398aaa9644722470d3548deab14a7
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-22 11:35:39 -0700

    All cases succefully implemented

--- Task5_Question2.cpp
+++ Task5_Question2.cpp
@@ -5,15 +5,18 @@ using namespace std;
 
 int main()
 {
+   /*Implementation of random number generator*/
    srand(time(nullptr));
-   int dayUntilExpiry=rand()%5 + 0;
+   int dayUntilExpiry=rand()%11 + 0;
 cout << "The number of days until expiry are: " << dayUntilExpiry << endl;
+  /*Implementation of switch cases*/
 switch(dayUntilExpiry){
    case 0: cout << "Your subscription has expired." <<endl;
    break;
  case 1: cout << "Your subscription expires within a day!" <<endl;
          cout << "Renew now and save 20%!" << endl;
          break;
+         /*Cases when number days are less than or equal to and out to be displayed*/
          case 2 : cout << "Your subscription expires in " << dayUntilExpiry << " days" <<endl;
                   cout << "Renew now and save 10%!"<< endl;
                   break; 
@@ -26,7 +29,20 @@ switch(dayUntilExpiry){
          case 5: cout << "Your subscription expires in " << dayUntilExpiry << " days" <<endl;
                   cout << "Renew now and save 10%!"<< endl;
                   break;
-
+                  /*Cases when numbers are less than or equal to 10 but greater than or euqal to 6*/
+         case 6: cout << "Your subscription will expire soon. Renew now!" << endl;   
+             break;
+           case 7: cout << "Your subscription will expire soon. Renew now!" << endl;
+           break;
+           case 8: cout << "Your subscription will expire soon. Renew now!" << endl;
+             break;
+             case 9: cout << "Your subscription will expire soon. Renew now!" << endl;
+             break;
+             case 10: cout << "Your subscription will expire soon. Renew now!" << endl;
+             break;
+             /*Case when number of days are equal to 11 the*/
+             case 11: cout << "You have an active subscription." << endl;
+             break;
 }
 return 0;
 }
