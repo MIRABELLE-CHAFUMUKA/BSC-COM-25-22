a27c8e026e6b7fcbdfca07daed41bde72b495599
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-22 14:05:24 -0700

    Declaration of array

---
+++ Task5_Question3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*Declaration of array*/
+    string element[8]={"B123","C234","A345","C15","B177","G3003","C235","B179"};
+
+    
+    return 0;
+}
