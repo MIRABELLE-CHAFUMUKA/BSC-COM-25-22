ad696e6aceca2dd75a70b9713bddbd6ad964416d
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-24 10:38:50 -0700

    Implementation of for loop with if statement. Question 3 success

--- Task5_Question3.cpp
+++ Task5_Question3.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main()
 {
     /*Declaration of array*/
-    string element[8]={"B123","C234","A345","C15","B177","G3003","C235","B179"};
-
-    
+    string elements[8] = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+    /*Implementation of for loop with if statement*/
+    for (int i = 0; i < 8; i++)
+    {
+        if (elements[i].substr(0, 1) == "B")
+        {
+            cout << elements[i] << endl;
+        }
+    }
     return 0;
 }
