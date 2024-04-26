991242bf7ecdb87b6b6f7ef9f082a2e65c82a4f1
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-26 04:46:18 -0700

    Dynamically allocating integers and strings

--- Task6_Question1.cpp
+++ Task6_Question1.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main (){
-    int numberOf
+    int numberOfElement;
+    int* dynamicArray = nullptr;
+    cout << "Please input number of your preference: " << endl;
+    cin>> numberOfElement;
+    cout << "String size is: "<< numberOfElement <<endl;
+
+    dynamicArray = new int [numberOfElement];
+
+string word;
+string* stringPointer = new string[8];
+cout << "Please write a string: " << endl;
+cin >> word;
+cout << "The string is "<< word<< endl;
+
+return 0;
 }
