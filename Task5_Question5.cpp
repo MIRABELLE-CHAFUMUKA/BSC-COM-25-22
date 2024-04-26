66992615d0490dd281c9f4df8eb5eff81876904b
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-04-26 03:30:34 -0700

    creation of Areas of shapes and prompting user, giving them expected output successful

--- Task5_Question5.cpp
+++ Task5_Question5.cpp
@@ -1,24 +1,66 @@
 #include <iostream>
 using namespace std;
-int AreaOfTriangle(int base, int height, int area);
 
-int main(){
-    int b;
-    int h;
-    int a;
-
-    int t = AreaOfTriangle(b, h, a);
-
-return 0;
+double triangleArea(double base, double height){
+    return 0.5 * base * height;
 }
-// Definition of function for area of triangle
-int AreaOfTriangle(int base, int height, int area){
-    cout << "please enter base value" << endl;
-    cin >> base;
-    cout << "please enter height value " << endl;
-    cin >> height;
-    area = 0.5 * base * height;
-    cout << "The area of the triangle is: " << area;
 
-    return area;
+double squareArea(double side){
+    return side * side;
+}
+
+double rectangleArea(double length, double width){
+    return length * width;
+}
+
+int main()
+{
+
+    int choice = 0;
+
+    while (choice != 4)
+    {
+        cout << "Please select the area of the shape to calculate" << endl;
+        cout << "1. Square" << endl;
+        cout << "2. Rectangle" << endl;
+        cout << "3. Triangle" << endl;
+        cout << "4. Quit Program" << endl;
+        cout << "Enter selection: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+        cout << "Enter the side of the square: " << endl;
+        double side;
+        cin >> side;
+        cout << "The area of the square is " << squareArea(side) << endl;
+            break;
+
+        case 2:
+         cout << "Enter the length of the rectangle: " << endl;
+        double length, width;
+        cin >> length;
+
+        cout << "Enter the width of the rectangle: " << endl;
+        cin >> width;
+        cout << "The area of the rectangle is " << rectangleArea(length, width) << endl;
+            break;
+
+         case 3:
+         cout << "Enter the base of the triangle: " << endl;
+        double base, height;
+        cin >> base;
+
+        cout << "Enter the height of the triangle: " << endl;
+        cin >> height;
+        cout << "The area of the triangle is " << triangleArea(base, height) << endl;
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    return 0;
 }
