cb02c9d22be9c4595803a9d715cca56d03f2ab86
“MIRABELLE-CHAFUMUKA” <bsc-com-25-22@unima.ac.mw>
2024-05-04 11:28:17 -0700

    Creation of class and members

---
+++ Lab_task_7/Task7_Question1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+// Creation of the class
+class Rectangle{
+    //Access specifier
+    private:
+
+    float length;
+    float width;
+// Constructor
+    Rectangle(){
+        length = 0;
+        width = 0;
+    }
+
+    public:
+    //member function
+    void area(float length, float width)
+};
+
+int main(){
+    Rectangle Object;
+
+}
